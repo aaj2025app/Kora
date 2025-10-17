cc17c4eb5fbfcd301d0e4a6ceb00a9b01a5897cc
Joaquin Vigil <AAJ2025app@gmail.com>
2025-10-16 21:37:01 -0400

    Initial Commit

---
+++ Kora/KoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoraApp.swift
+//  Kora
+//
+//  Created by Joaquin vigil on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct KoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
